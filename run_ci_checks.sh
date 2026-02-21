165355cfd2c37e210a91695261a2d399e531033f
Eric Hu <ericlehaohu@gmail.com>
2026-02-20 22:26:54 -0500

    set up motion diffusino model and pyproject.toml to have mdm dependencies

--- run_ci_checks.sh
+++ run_ci_checks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./run_autoformat.sh
-mypy .
-pytest . --pylint -m pylint --pylint-rcfile=.pylintrc
+mypy . --exclude src/uncertain_feedback/motion_generators/mdm/motion-diffusion-model
+pytest . --ignore=src/uncertain_feedback/motion_generators/mdm/motion-diffusion-model --pylint -m pylint --pylint-rcfile=.pylintrc
 pytest tests/

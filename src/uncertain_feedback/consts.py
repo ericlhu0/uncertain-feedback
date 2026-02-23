5ca1cddad8bdbc8fadf863fbabb5d655325aa13e
Eric Hu <ericlehaohu@gmail.com>
2026-02-22 23:06:32 -0500

    add motion generation for isolated left arm, ourside of motion-diffusion-model repo

--- src/uncertain_feedback/consts.py
+++ src/uncertain_feedback/consts.py
@@ -3,15 +3,11 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent
+MDM_ROOT = PROJECT_ROOT / "motion_generators" / "mdm"
 MDM_MODEL_WEIGHTS_PATH = (
-    PROJECT_ROOT
-    / "motion-generators"
-    / "mdm"
+    MDM_ROOT
     / "motion-diffusion-model"
     / "save"
     / "humanml_enc_512_50steps"
     / "model000750000.pt"
 )
-
-print(PROJECT_ROOT)
-print(MDM_MODEL_WEIGHTS_PATH)
